147520593f7df5b8eb273e16d31932fff4d574a3
Unknown <Tori7809@gmail.com>
2019-12-18 21:20:16 -0700

    Data Sorter script

---
+++ WinterGameScene/Assets/Scripts/DataDisplay.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditorInternal;
+using UnityEngine;
+
+public class DataDisplay : MonoBehaviour
+{
+
+    public DataSorter card;
+    // Start is called before the first frame update
+    void Start()
+    {
+        card.Print();
+    }
+
+}
